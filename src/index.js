c938ea7630d065c6bf9aa593302b847a00f54614
Joao Otavio Chrisóstomo Barbosa <jobarbosa@sfiec.org.br>
2020-04-15 14:42:31 -0300

    Structure the project and add handlebars module

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 const path = require('path');
-
 const express = require('express');
 
-console.log(__dirname);
-// console.log(path.join(__dirname))
-
 const app = express();
+const publicDirectory = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectory));
 
 // const url = 'http://api.weatherstack.com/current?access_key=d5f217766402b840cb44aea9deb10e15&query=-3.7899266,-38.5889879&units=m'
-
-app.get('/', (request, response) => {
-    response.send('Hello World')
-});
-
-app.get('/about', (request, response) => {
-    response.send('<h1>About the site</h1>')
-});
 
 app.get('/weather', (request, response) => {
     response.send({
@@ -23,17 +15,6 @@ app.get('/weather', (request, response) => {
         location: 'Philadelphia'
     })
 });
-
-app.get('/help', (request, response) => {
-    response.send([{
-        name: "Joao",
-        age: 27
-    },
-    {
-        name: "Joao",
-        age: 27
-    }])
-})
 
 app.listen(3000, () => {
     console.log('Server UP and running')
