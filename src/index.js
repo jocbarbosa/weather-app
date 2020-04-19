40ca793e863e1fff666576a3f2c14ae1cdeae4ac
Joao C Barbosa <joaao@outlook.com>
2020-04-19 12:41:15 -0300

    refactor:refactor the weather route to get geocode informatin from API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const req = require('request');
 
 const app = express();
 
@@ -13,7 +14,7 @@ const partialsPath = path.join(__dirname, '../templates/partials');
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
-hbs.registerPartials(partialsPath)
+hbs.registerPartials(partialsPath);
 
 // Setup static directory
 app.use(express.static(publicDirectory));
@@ -40,20 +41,53 @@ app.get('/help', (request, response) => {
     });
 });
 
+app.get('/weather', (request, response) => {
+
+    const { latitude, longitude } = request.query;
+    const url = 'http://api.weatherstack.com/current?access_key=d5f217766402b840cb44aea9deb10e15&query=' + latitude + ',' + longitude + '&units=m';
+
+
+    req({ url: url, json: true }, (err, resp) => {
+        if (!latitude || !longitude) {
+            response.send({
+                error: 'You need to give a latitude and longitude'
+            });
+        } else if (err) {
+            response.send({
+                error: 'Unable to connect to location service!'
+            });
+        } else if (resp.body.success === false) {
+            response.send({
+                error: 'Unable to find this location. Try another one!'
+            });
+        } else {
+            response.send({
+                givenData: {
+                    latitude: resp.body.location.lat,
+                    longitude: resp.body.location.lon,
+                },
+                location: {
+                    name: resp.body.location.name,
+                    country: resp.body.location.country,
+                    region: resp.body.location.region,
+                    timezone: resp.body.location.timezone_id
+                },
+                weather: {
+                    temperature: resp.body.current.temperature,
+                    description: resp.body.current.weather_descriptions,
+                    wind_speed: resp.body.current.wind_speed
+                }
+            });
+        }
+    });
+});
+
+
 app.get('*', (request, response) => {
     response.render('404', {
         title: '404',
         errorMessage: 'Page Nout Found'
     });
-});
-
-// const url = 'http://api.weatherstack.com/current?access_key=d5f217766402b840cb44aea9deb10e15&query=-3.7899266,-38.5889879&units=m'
-
-app.get('/weather', (request, response) => {
-    response.send({
-        forecast: 'It\' snowing',
-        location: 'Philadelphia'
-    })
 });
 
 app.listen(3000, () => {
